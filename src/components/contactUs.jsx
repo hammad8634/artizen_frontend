f3606cb4bc3a3914f16a86e0e4aa0d7e5302f0b1
hp <57030669+hammad8634@users.noreply.github.com>
2023-05-13 16:09:30 +0500

    Contact Us Done

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -20,41 +20,9 @@ const ContactUs = () => {
   return (
     <Navbar>
       <div className="main-div">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-          {/* Left Side */}
-          <div>
-            <h2 className="text-2xl font-bold mb-4">Get in touch</h2>
-            <p className="text-gray-600 mb-8">
-              Proin volutpat consequat porttitor cras nullam gravida at. Orci
-              molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
-              Arcu sed malesuada et magna.
-            </p>
-            <div className="flex items-center mb-4">
-              <PlayPauseIcon className="w-6 h-6 text-indigo-600 mr-2" />
-              <div>
-                <h3 className="text-lg font-semibold mb-2">Address</h3>
-                <p>545 Mavis Island</p>
-                <p>Chicago, IL 99191</p>
-              </div>
-            </div>
-            <div className="flex items-center mb-4">
-              <PhoneIcon className="w-6 h-6 text-indigo-600 mr-2" />
-              <div>
-                <h3 className="text-lg font-semibold mb-2">Telephone</h3>
-                <p>+1 (555) 234-5678</p>
-              </div>
-            </div>
-            <div className="flex items-center">
-              <MinusSmallIcon className="w-6 h-6 text-indigo-600 mr-2" />
-              <div>
-                <h3 className="text-lg font-semibold mb-2">Email</h3>
-                <p>hello@example.com</p>
-              </div>
-            </div>
-          </div>
-
-          {/* Right Side */}
-          <div className="isolate bg-white px-6 py-24 sm:py-10 lg:px-7">
+        <div className="grid grid-cols-1 lg:grid-cols-2 ">
+          {/* Left Side*/}
+          <div className="isolate bg-white px-6 py-24 sm:py-10 lg:px-7 p-7">
             <div
               className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
               aria-hidden="true"
@@ -68,10 +36,10 @@ const ContactUs = () => {
               />
             </div>
             <div className="mx-auto max-w-2xl text-center">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
                 Contact Us
               </h2>
-              <p className="mt-2 text-lg leading-8 text-gray-600">
+              <p className="mt-2 text-lg leading-8 text-gray-60 p-5">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
                 laudantium similique, repellendus assumenda eveniet nulla
                 voluptates provident, nam adipisci debitis ratione qui ad
@@ -81,7 +49,7 @@ const ContactUs = () => {
             <form
               action="#"
               // method="POST"
-              className="mx-auto mt-16 max-w-xl sm:mt-20"
+              className="mx-auto mt-10 max-w-xl sm:mt-10"
             >
               <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                 <div>
@@ -118,23 +86,7 @@ const ContactUs = () => {
                     />
                   </div>
                 </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="company"
-                    className="block text-sm font-semibold leading-6 text-gray-900"
-                  >
-                    Company
-                  </label>
-                  <div className="mt-2.5">
-                    <input
-                      type="text"
-                      name="company"
-                      id="company"
-                      autoComplete="organization"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
+
                 <div className="sm:col-span-2">
                   <label
                     htmlFor="email"
@@ -169,7 +121,7 @@ const ContactUs = () => {
                         name="country"
                         className="font-medium h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                       >
-                        <option className="font-medium">PK</option>
+                        <option className="font-medium">+92 |</option>
                       </select>
                       <ChevronDownIcon
                         className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
@@ -240,6 +192,39 @@ const ContactUs = () => {
                 </button>
               </div>
             </form>
+          </div>
+          {/* Right Side */}
+          <div className="isolate bg-white px-6 py-24 sm:py-10 lg:py-20 flex justify-center ">
+            <div className="border rounded-lg h-full p-10 bg-gray-100">
+              <h2 className="text-3xl font-bold mb-4">Get in touch</h2>
+              <p className="text-gray-600 mb-8">
+                Proin volutpat consequat porttitor cras nullam gravida at. Orci
+                molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
+                Arcu sed malesuada et magna.
+              </p>
+              <div className="flex items-center mb-8">
+                <PlayPauseIcon className="w-6 h-6 text-indigo-600 mr-2" />
+                <div>
+                  <h3 className="text-lg font-semibold mb-2">Address</h3>
+                  <p>545 Mavis Island</p>
+                  <p>Chicago, IL 99191</p>
+                </div>
+              </div>
+              <div className="flex items-center mb-8">
+                <PhoneIcon className="w-6 h-6 text-indigo-600 mr-2" />
+                <div>
+                  <h3 className="text-lg font-semibold mb-2">Telephone</h3>
+                  <p>+1 (555) 234-5678</p>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <MinusSmallIcon className="w-6 h-6 text-indigo-600 mr-2" />
+                <div>
+                  <h3 className="text-lg font-semibold mb-2">Email</h3>
+                  <p>hello@example.com</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
